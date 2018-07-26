7276663b642ec97bbf2638b7c922625626ccff93
Drew <drewgill89@gmail.com>
2018-07-26 16:18:35 -0400

    Debugging Player Controller

--- DreamTeamExperience/Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/FirstPersonController.cs
+++ DreamTeamExperience/Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/FirstPersonController.cs
@@ -206,7 +206,8 @@ namespace UnityStandardAssets.Characters.FirstPerson
             // Read input
             float horizontal = CrossPlatformInputManager.GetAxis("Horizontal");
             float vertical = CrossPlatformInputManager.GetAxis("Vertical");
-
+            Debug.Log(horizontal);
+            Debug.Log(vertical);
             bool waswalking = m_IsWalking;
 #if !MOBILE_INPUT
             // On standalone builds, walk/run speed is modified by a key press.
