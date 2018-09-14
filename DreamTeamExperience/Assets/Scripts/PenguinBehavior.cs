ac049dd0f7c76c7736215d56a5bec8353ae3f0eb
sydneynachinger <32098695+sydneynachinger@users.noreply.github.com>
2018-09-13 22:49:17 -0400

    penguin walks around but goes through things right now

--- DreamTeamExperience/Assets/Scripts/PenguinBehavior.cs
+++ DreamTeamExperience/Assets/Scripts/PenguinBehavior.cs
@@ -2,67 +2,70 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PenguinBehavior : MonoBehaviour {
 
-    public float moveSpeed = 3f;
-    public float rotSpeed = 100f;
-
-    private bool isWandering = false;
-    private bool isRotatingLeft = false;
-    private bool isRotatingRight = false;
-    private bool isWalking = false;
+public class PenguinBehavior : MonoBehaviour
+{
 
 
-	// Use this for initialization
-	void Start () {
-        
-	}
-	
-	// Update is called once per frame
-	void Update () {
 
-        if(isWandering == false){
-            StartCoroutine(Wander());
-        }
-        else if(isRotatingRight == true){
-            transform.Rotate(transform.up* Time.deltaTime);
-        }
-        else if (isRotatingLeft == true)
-        {
-            transform.Rotate(transform.up * -Time.deltaTime);
-        }
-
-        transform.position = transform.forward * moveSpeed * Time.deltaTime;
-
-
-	}
-    IEnumerator Wander(){
-
-        int rotTime = Random.Range(1, 3);
-        int rotateWait = Random.Range(1, 4);
-        int rotateLorR = Random.Range(1, 2);
-        int walkWait = Random.Range(1, 4);
-        int walkTime = Random.Range(1, 5);
-
-        isWandering = true;
-
-        yield return new WaitForSeconds(walkWait);
-        isWalking = true;
-        yield return new WaitForSeconds(walkTime);
-        isWalking = false;
-        yield return new WaitForSeconds(rotateWait);
-        if(rotateLorR == 1){
-            isRotatingRight = true;
-            yield return new WaitForSeconds(rotTime);
-            isRotatingRight = false;
-        }
-        if (rotateLorR == 2)
-        {
-            isRotatingLeft = true;
-            yield return new WaitForSeconds(rotTime);
-            isRotatingLeft = false;
-        }
-        isWandering = false;
-
+    public Transform myTransform;
+    public float speed = 1.0f;
+    Vector3 vel = new Vector3(0,0,0);
+    public float curTime = 0f;
+    public float switchDirection = 3f;
+    // Use this for initialization
+    void Start()
+    {
+        SetVel();
     }
+
+    void SetVel(){
+        if (Random.value > .5)
+        {
+            vel.x = 10* 10 * Random.value;
+        }
+        else
+        {
+            vel.x = -10 * 10 * Random.value;
+        }
+        if (Random.value > .5)
+        {
+            vel.z = 10 * 10 * Random.value;
+        }
+        else
+        {
+            vel.z = -10 * 10 * Random.value;
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (curTime < switchDirection)
+        {
+            curTime += 1 * Time.deltaTime;
+        }
+        else
+        {
+            SetVel();
+            if (Random.value > .5)
+            {
+                switchDirection += Random.value;
+            }
+    else {
+                    switchDirection -= Random.value;
+                }
+                if (switchDirection < 1)
+                {
+                    switchDirection = 1 + Random.value;
+                }
+                curTime = 0;
+            }
+
+        }
+
+	private void FixedUpdate()
+	{
+        transform.Translate(vel * Time.deltaTime);
+	}
+
 }
